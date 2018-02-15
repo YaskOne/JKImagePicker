913e906611482a1da1f70947f21d52c652db153e
Tristan Leblanc <tristan@moosefactory.eu>
2018-02-15 19:46:30 +0100

    Added Composition Dictionary

--- JKImagePicker/Source/Model/JKSplitComposition.swift
+++ JKImagePicker/Source/Model/JKSplitComposition.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreGraphics
+import JackFoundation
 
 public class JKComposition: JKImageRepresentable {
 	
@@ -46,12 +47,33 @@ public class JKComposition: JKImageRepresentable {
 
 	public var image1: JKImage?
 	public var image2: JKImage?
+	
+	public func dictionary(image1UID: String, image2UID: String) -> JsonDict {
+		let img1Dict: JsonDict = ["uid" : image1UID]
+		let img2Dict: JsonDict = ["uid" : image2UID]
+		let splitDict: JsonDict = ["image1": img1Dict, "image2": img2Dict]
+		let featuresDict: JsonDict = ["base" : splitDict]
+		let info: JsonDict = ["size": frame.size, "features" : featuresDict]
+		
+		return info
+	}
+
 }
 
 public class JKSplitComposition : JKComposition {
 	public var angle: CGFloat = 0
 	public var center: CGPoint = CGPoint(x: 0.5, y: 0.5)
 
+	override public func dictionary(image1UID: String, image2UID: String) -> JsonDict {
+		let img1Dict: JsonDict = ["uid" : image1UID]
+		let img2Dict: JsonDict = ["uid" : image2UID]
+		let splitDict: JsonDict = ["angle" : angle, "center" : center, "image1": img1Dict, "image2": img2Dict]
+		let featuresDict: JsonDict = ["split" : splitDict]
+		let info: JsonDict = ["size": frame.size, "features" : featuresDict]
+		
+		return info
+		}
+	
 	public override var image: UIImage? { get {
         guard let img1 = image1?.image?.cgImage, let img2 = image2?.image?.cgImage, let frame = image1?.frame else {
             return nil
@@ -67,5 +89,5 @@ public class JKSplitComposition : JKComposition {
 		self.center = center
 	}
 	
-
+	
 }
